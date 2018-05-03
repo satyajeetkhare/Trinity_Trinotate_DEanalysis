2670b37a0360b20bf16f1dc467c880006caff558
satyajeetkhare <36985285+satyajeetkhare@users.noreply.github.com>
2018-05-03 11:43:23 +0530

    Update analyze_diff_expr.r

--- analyze_diff_expr.r
+++ analyze_diff_expr.r
@@ -9,7 +9,7 @@ go_annotations <- read.csv("DE_analysis/go_annotations.txt", sep = NULL, header 
 # Create data object for annot_feature_map
 annot_feature_map <- read.csv("DE_analysis/annot_feature_map.txt", sep = "\t", header = FALSE)
 
-# Modify Trinity_gene_lengths data object to by adding a third column with Annotations
+# Modify Trinity_gene_lengths data object by adding a third column with Annotations
 Trinity_gene_lengths_mod <- left_join(Trinity_gene_lengths, annot_feature_map, by = c("X.gene_id" = "V1"))
 
 # Modify third column by replacing NA with Trinity gene IDs
@@ -24,14 +24,16 @@ Trinity_gene_lengths_mod$V2 <- NULL
 # Write a modified gene length file. This file will be used for analysis of differential expression
 write.table(Trinity_gene_lengths_mod, file = "Trinity_gene_lengths_mod.txt", quote = FALSE, row.names = FALSE, sep = "\t")
 
-# Modify go_annotations
+# Modify go_annotations by adding a third column with Annotations
 go_annotations_mod <- left_join(go_annotations, annot_feature_map, by = c("V1" = "V1"))
 
-# 
+# Relace the "V1" values with values in the third column (V2.y)
 go_annotations_mod[, "V1"] <- go_annotations_mod$V2.y
 
+# Delete the third column. Its no longer required.
 go_annotations_mod$V2 <- NULL
 
+# Write a modified go annotation file. This file will be used for analysis of differential expression
 write.table(go_annotations_mod, file = "go_annotations_mod.txt", quote = FALSE, row.names = FALSE, sep = "\t")
 
 # Use "Trinity_gene_lengths_mod.txt" in place of "Trinity.gene_lengths.txt" 
